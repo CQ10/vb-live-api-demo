a9496037190c176c48b54c5137e2b29a98597a68
Balthazar Gronon <bgronon@gmail.com>
2019-05-30 15:07:51 -0700

    Refactor architecture to support backup events

--- core.js
+++ core.js
@@ -2,34 +2,52 @@ const socketCluster = require('socketcluster-client')
 
 const options = {
   host: 'live.viewblock.io:3001',
+  // host: 'localhost:4041',
   autoConnect: true,
   autoReconnect: true,
   autoReconnectOptions: {
-    initialDelay: 10e3,
-    randomness: 10e3,
-    multiplier: 1.5,
-    maxDelay: 60e3,
+    initialDelay: 1e3,
+    randomness: 5e3,
+    multiplier: 1.1,
+    maxDelay: 20e3,
   },
 }
 
-const addPrefix = str => (str.startsWith('0x') ? str : `0x${str}`)
-
-module.exports = ({ key, secret, onError = f => f } = {}) => {
+module.exports = ({ key, secret, onError = f => f } = {}, onReady) => {
   if (!key) {
     throw new Error('An api key is required.')
   }
 
+  const tss = {}
+
   const socket = socketCluster.create(options)
 
-  socket.emit('login', { key, secret })
+  const subscribe = (payload, cb) => {
+    const { chain, address, event } = payload
+    const key = `${chain}:${address ? `${address}:` : ''}${event}`
+
+    if (tss[key]) {
+      socket.emit('backup', { ...payload, ts: tss[key] }, (err, res = []) => {
+        res.forEach(cb)
+        socket.subscribe(key)
+      })
+
+      return
+    }
+
+    socket.subscribe(key).watch(data => {
+      tss[key] = data.ts
+      cb(data)
+    })
+  }
+
+  socket.on('connect', () => {
+    socket.emit('login', { key, secret })
+  })
+
+  socket.on('authenticate', () => {
+    onReady({ socket, subscribe })
+  })
 
   socket.on('error', onError)
-
-  return {
-    socket,
-    subscribe: ({ chain, address, event }, cb) => {
-      const key = `${chain}:${address ? `${addPrefix(address)}:` : ''}${event}`
-      socket.subscribe(key).watch(cb)
-    },
-  }
 }
--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 const init = require('./core')
 
-const { socket, subscribe } = init({
-  key: '',
-  secret: '',
-})
-
-socket.on('connect', () => {
-  console.log('[Connected]')
-})
-
-socket.on('authenticate', () => {
-  console.log('[Authenticated]')
-
-  subscribe(
-    {
+init(
+  {
+    key: 'f7a2a04d3aa238a88db7ed4aa9dda4757e3f8176195066132f458b8b34e18750',
+    secret: '7251f0b9555244a3c3158e61c170a2bcbc757d017072be29330a0737ba7aba2e',
+  },
+  ({ subscribe }) => {
+    const subInfo = {
       chain: 'zilliqa',
       address: '11d3cfe90a863245a00cff9d069ffc27585ee764',
       event: 'contractEvent',
-    },
-    data => {
+    }
+
+    subscribe(subInfo, data => {
       console.log('[ContractEvent]', data)
-    },
-  )
-})
+    })
+  },
+)
